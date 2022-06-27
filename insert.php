8b384bcf8707fa4ca08de99cd36904e000eb762d
FaizanNabi-hub <73012846+FaizanNabi-hub@users.noreply.github.com>
2022-06-27 10:44:49 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,40 @@
+<?php
+$psname = filter_input(INPUT_POST, 'psname' );
+$email = filter_input(INPUT_POST, 'email' );
+$phone = filter_input(INPUT_POST, 'phone' );
+$address = filter_input(INPUT_POST, 'address' );
+$review = filter_input(INPUT_POST, 'review' );
+
+if(!empty($psname) || !empty($email) || !empty($phone) || !empty($address) || !empty($review))
+{
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = " ";
+$dbname = "phpmyadmin";
+
+
+// create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);\
+if(mysqli_connect_error()){
+    die('Connect Error(' .mysqli_connect_errno().')'.mysqli_connect_error());
+}
+else
+{
+    $sql = "INSERT INTO  coffee webpage user details (psname,email,phone,address,review)
+    values ('$psname','$email','$phone','$address','$review')";
+    if($conn->query($sql))
+    {
+        echo "NEW RECORD INSERTED SUCCESSFULLY";
+    }
+    else{
+        echo "ERROR : ". &sql ."<br>". $conn->error;
+    }
+    $conn->close();
+
+}
+else
+{
+    echo" UNSUCCESSFUL !!!!";
+    die();
+}
+?>
